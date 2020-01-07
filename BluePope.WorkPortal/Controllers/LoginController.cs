1d58b7d8a34d171977bf13f84e76937559878f88
안현모 <blue_pope@hotmail.com>
2020-01-07 09:24:50 +0900

    로그인 api controllor로 변경 razor login 테스트

---
+++ BluePope.WorkPortal/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BluePope.WorkPortal.Controllers
+{
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/api/login")]
+        public async Task<IActionResult> Login([FromForm] MLogin input)
+        {
+            string returnUrl = Url.Content("~/");
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+            identity.AddClaim(new Claim(ClaimTypes.Name, input.UserName));
+            identity.AddClaim(new Claim(ClaimTypes.Role, "Administrator"));
+
+            var principal = new ClaimsPrincipal(identity);
+
+            try
+            {
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties
+                {
+                    IsPersistent = false, //로그인 쿠키 영속성 (브라우저 종료시 유지) 여부
+                    ExpiresUtc = DateTime.UtcNow.AddDays(7), //7일간 미접속시 쿠키 만료
+                    AllowRefresh = true, //갱신여부
+                    RedirectUri = this.Request.Host.Value
+                });
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+            return Redirect("/");
+        }
+
+        [Route("/api/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            
+            return Redirect("/");
+        }
+    }
+
+    public class MLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
